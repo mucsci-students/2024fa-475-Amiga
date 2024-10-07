f10566a8a81184aa001a396d7c745d5b51441821
unknown <djpiruch@millersville.edu>
2024-10-07 01:54:20 -0400

    Level 1 demo

---
+++ Amiga/Assets/Tilemap Related Things/portal/portal.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    private HashSet<GameObject> portalObjects = new HashSet<GameObject>();
+
+    [SerializeField] private Transform destination;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (portalObjects.Contains(collision.gameObject))
+        {
+            return;
+        }
+
+        if (destination.TryGetComponent(out Portal destinationPortal))
+        {
+            destinationPortal.portalObjects.Add(collision.gameObject);
+        }
+
+        collision.transform.position = destination.position;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        portalObjects.Remove(collision.gameObject);
+    }
+}
