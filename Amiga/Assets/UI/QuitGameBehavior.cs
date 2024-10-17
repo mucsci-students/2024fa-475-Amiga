d6f31e0e8537d6efe250966ba8329f70c4580152
Lincoln <lincolncraddock@gmail.com>
2024-10-17 02:32:06 -0400

    quit

--- Amiga/Assets/UI/QuitGameBehavior.cs
+++ Amiga/Assets/UI/QuitGameBehavior.cs
@@ -6,19 +6,12 @@ public class QuitGameBehavior : MonoBehaviour
 {
     public void Quit ()
     {
-        print ("start");
         StartCoroutine (QuitCoroutine ());
     }
 
     private IEnumerator QuitCoroutine ()
     {
-        yield return new WaitForSeconds (0.5f);
-#if UNITY_EDITOR
-print ("editor");
-        UnityEditor.EditorApplication.isPlaying = false;
-#else
-print ("application");
+        yield return new WaitForSecondsRealtime (0.5f);
         Application.Quit();
-#endif
     }
 }
